99a565c8b4f75098f2ff7d7f95a0e7c2f8c44d16
Sarasecellterra <75311970+Sarasecellterra@users.noreply.github.com>
2021-03-10 10:07:58 +0000

    Update A.js

--- A.js
+++ A.js
@@ -2,3 +2,5 @@ let the_horror = [7, 9, 12, 21, 47, 67, 77, 79, 81, 90, 97]
 let startindex = 0;
 let endindex array.length - 1;
 let middleindex = math.floor ((startindex + endindex)/2)
+while (startindex >= endindex)
+  console.log(endindex)
