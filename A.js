a13897231115ba1f0c1b27665de355bae2914c2e
Sarasecellterra <75311970+Sarasecellterra@users.noreply.github.com>
2021-03-10 10:14:25 +0000

    Update A.js

--- A.js
+++ A.js
@@ -1,4 +1,4 @@
-let the_horror = [7, 9, 12, 21, 47, 67, 77, 79, 81, 90, 97]
+let thehorror = [7, 9, 12, 21, 47, 67, 77, 79, 81, 90, 97];
 let startindex = 0;
 let endindex array.length - 1;
 let middleindex = math.floor ((startindex + endindex)/2)
